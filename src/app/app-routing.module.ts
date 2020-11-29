3d1ccc7be0fe8b05515e3c1ad592a94a182137c4
Juan Bustos <juanbv81@gmail.com>
2020-11-29 12:16:50 +0100

    Fin sección 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,31 @@
 import { NgModule } from '@angular/core';
 //import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
 
-const routes: Routes = [
-  { 
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },  
-    
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-    
+// Modulos
+import { Routes, RouterModule } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+// Componentes
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+const routes: Routes = [      
+
+  // path: '/dashboard' PagesRouting
+  // path: '/progress' PagesRouting
+  // path: '/grafica1' PagesRouting
+  // path: '/login' AuthRouting
+  // path: '/register' AuthRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // ruta por defecto, al dashboard
   { path: '**', component: NopagefoundComponent }
 ]
 
 @NgModule({
-  declarations: [],
   imports: [
     //CommonModule (viene del ng g m appRouting --flat, y no es necesario)
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,   // Rutas de componentes de pages
+    AuthRoutingModule     // Rutas de auth
   ],
   exports: [ RouterModule ]
 })
