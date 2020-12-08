2b55cba8dbd89dcecd90b2af6cd69ea13462c35e
Juan Bustos <juanbv81@gmail.com>
2020-12-08 19:56:36 +0100

    Fin sección 6

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; // para que funcione <router-outlet></router-outlet> en pages.component.html
+import { FormsModule } from '@angular/forms'; // hace que funcione [(ngModule)] en inputs
+
+//import { ChartsModule } from 'ng2-charts'; // https://valor-software.com/ng2-charts/#/GeneralInfo
 
 // Modulos
 import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router'; // para que funcione <router-outlet></router-outlet> en pages.component.html
+import { ComponentsModule } from '../components/components.module';
 
 // Directorio: pages
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
-
-
 
 
 @NgModule({
@@ -29,8 +31,10 @@ import { Grafica1Component } from './grafica1/grafica1.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    RouterModule,
     SharedModule,
-    RouterModule
+    ComponentsModule 
   ]
 })
 export class PagesModule { }
